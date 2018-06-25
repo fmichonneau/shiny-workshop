230062c75769c68f6a53e5118ae93247bcdebc32
François Michonneau <francois.michonneau@gmail.com>
2018-06-25 14:46:12 -0400

    use split layout

--- ui.R
+++ ui.R
@@ -3,16 +3,24 @@ fluidPage(
     dateRangeInput("workshop_dates", "What are the dates for the workshops?"),
 
     h3("Start time for the workshop: "),
-    numericInput("time_start_hour", "Start time (hours): ", value = 9, min = 1, max = 11),
-    selectInput("time_start_min", "Start time (minutes):",
-                choices = c("00", "15", "30", "45")),
-    radioButtons("time_start_ampm", "AM/PM", choices = c(am = "am", pm = "pm"), inline = TRUE),
+    splitLayout(
+        numericInput("time_start_hour", "Start time (hours): ",
+                     value = 9, min = 1, max = 11),
+        selectInput("time_start_min", "Start time (minutes):",
+                    choices = c("00", "15", "30", "45")),
+        radioButtons("time_start_ampm", "AM/PM",
+                     choices = c(am = "am", pm = "pm"), inline = TRUE)
+    ),
 
     h3("End time for the workshop: "),
-    numericInput("time_stop_hour", "Stop time (hours): ", value = 5, min = 1, max = 11),
-    selectInput("time_stop_min", "Stop time (minutes):",
-                 choices = c("00", "15", "30", "45")),
-    radioButtons("time_stop_ampm", "AM/PM", choices = c(am = "am", pm = "pm"), selected = "pm", inline = TRUE),
+    splitLayout(
+        numericInput("time_stop_hour", "Stop time (hours): ",
+                     value = 5, min = 1, max = 11),
+        selectInput("time_stop_min", "Stop time (minutes):",
+                    choices = c("00", "15", "30", "45")),
+        radioButtons("time_stop_ampm", "AM/PM",
+                     choices = c(am = "am", pm = "pm"), selected = "pm", inline = TRUE)
+    ),
 
     h3("Where is the workshop taking place?"),
     textInput("short_name", "Short name (slug):"),
